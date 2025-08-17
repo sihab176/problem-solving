22df7954dada10f88495c59b4fee597a753a10eb
unknown <shariar2739@gmail.com>
2025-08-17 23:01:59 +0600

    solve the problem 2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-//? Problem 1: Reverse a String 
+//? Problem 1: Reverse a String    ========>
 
 const name= "hello"
 let reverse=""
 for(let i = name.length-1 ; i >=0 ; i--){
     reverse =reverse + name[i]
 }
-console.log(reverse);
+console.log("reverse",reverse);
+
+
+
+
+//? Problem 2: Count Vowels in a String =====>
+const checkVowels="programming"
+const demoVowels = "aeiouAEIOU"
+let count=0
+for( let j =0 ; j <checkVowels.length ; j++ ){
+    if(checkVowels.includes(demoVowels[j])){
+    count++
+    }
+}
+console.log("Total vowels",count);
+
 
 
 
